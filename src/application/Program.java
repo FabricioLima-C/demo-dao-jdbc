5d3deb5e7667819c40a6a9b343bdef9901ed3e9f
FabricioLima-C <fabriciocabral07@gmail.com>
2020-02-29 19:55:36 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.util.Locale;
+
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		
+		Department obj = new Department("Logistica");
+		System.out.println(obj);
 
 	}
 
